0d6fe74cb3a3c11d19cce3953d6a7c896ca8cb61
st52531 <43739550+st52531@users.noreply.github.com>
2019-02-20 21:16:50 +0100

    cv_01_p_04 updated

--- ICSHP_cv_01/ICSHP_cv_01_p_04/Program.cs
+++ ICSHP_cv_01/ICSHP_cv_01_p_04/Program.cs
@@ -11,6 +11,22 @@ namespace ICSHP_cv_01_p_04
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            int rndNum = rnd.Next(100);
+            int guess;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Guess generated number");
+                guess = int.Parse(Console.ReadLine());
+                Console.WriteLine(rndNum);
+                if (guess == rndNum)
+                {
+                    Console.WriteLine("You are right, the number is " + rndNum + ".");
+                    return;
+                }
+                Console.WriteLine("That's not the number." + "Attempts left: " + (9-i));
+            }
+            Console.WriteLine("Game lost. You do not have any more attempts.");
         }
     }
 }
