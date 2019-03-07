b5b44e90f98233132957b0b3381d7b867e994df8
st52531 <st52531@student.upce.cz>
2019-03-07 23:39:39 +0100

    BaseLib

--- ICSHP_cv_02/BaseLib/MathConvertor.cs
+++ ICSHP_cv_02/BaseLib/MathConvertor.cs
@@ -6,21 +6,121 @@ using System.Threading.Tasks;
 
 namespace BaseLib
 {
+    /// <summary>
+    /// Class for conversion between different mathematical systems
+    /// </summary>
     public static class MathConvertor
     {
+        public static readonly Dictionary<char, int> RomanNumberDictionary;
+        public static readonly Dictionary<int, string> NumberRomanDictionary;
+
+        /// <summary>
+        /// Converts decimal numbers to binary
+        /// </summary>
+        /// <param name="dec"></param>
+        /// <returns>binary number in string</returns>
         public static string DecToBin(int dec)
         {
             return Convert.ToString(dec, 2);
         }
+
+        /// <summary>
+        /// Converts binary numbers to decimal
+        /// </summary>
+        /// <param name="bin"></param>
+        /// <returns>number in int</returns>
         public static int BinToDec(string bin)
         {
             return Convert.ToInt32(bin, 2);
         }
-        public static void DecToRoman()
+
+        /// <summary>
+        /// Converts decimal numbers to roman
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>roman number as string</returns>
+        public static string DecToRoman(int number)
         {
+            var roman = new StringBuilder();
+
+            foreach (var item in NumberRomanDictionary)
+            {
+                while (number >= item.Key)
+                {
+                    roman.Append(item.Value);
+                    number -= item.Key;
+                }
+            }
+
+            return roman.ToString();
         }
-        public static void RomanToDec()
+
+        /// <summary>
+        /// Converts roman numbers to decimal
+        /// </summary>
+        /// <param name="roman"></param>
+        /// <returns>number in string</returns>
+        public static int RomanToDec(string roman)
         {
+            int total = 0;
+
+            int current, previous = 0;
+            char currentRoman, previousRoman = '\0';
+
+            for (int i = 0; i < roman.Length; i++)
+            {
+                currentRoman = roman[i];
+
+                previous = previousRoman != '\0' ? RomanNumberDictionary[previousRoman] : '\0';
+                current = RomanNumberDictionary[currentRoman];
+
+                if (previous != 0 && current > previous)
+                {
+                    total = total - (2 * previous) + current;
+                }
+                else
+                {
+                    total += current;
+                }
+
+                previousRoman = currentRoman;
+            }
+
+            return total;
         }
+
+        /// <summary>
+        /// Contains dictionaries for conversion between decimal and roman system
+        /// </summary>
+        static MathConvertor()
+        {
+            RomanNumberDictionary = new Dictionary<char, int>
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 },
+        };
+
+            NumberRomanDictionary = new Dictionary<int, string>
+        {
+            { 1000, "M" },
+            { 900, "CM" },
+            { 500, "D" },
+            { 400, "CD" },
+            { 100, "C" },
+            { 50, "L" },
+            { 40, "XL" },
+            { 10, "X" },
+            { 9, "IX" },
+            { 5, "V" },
+            { 4, "IV" },
+            { 1, "I" },
+        };
+        }
+
     }
 }
--- ICSHP_cv_03/ICSHP_cv_03_p_02/Program.cs
+++ ICSHP_cv_03/ICSHP_cv_03_p_02/Program.cs
@@ -11,8 +11,39 @@ namespace ICSHP_cv_03_p_02
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Conversion of decimal to binary system.");
+            Console.WriteLine("-------------------------------------------------");
             Console.WriteLine(MathConvertor.DecToBin(8));
+            
+
+            Console.WriteLine("\n-------------------------------------------------");
+            Console.WriteLine("Conversion of binary to decimal system.");
+            Console.WriteLine("-------------------------------------------------");
             Console.WriteLine(MathConvertor.BinToDec("0101"));
+            
+
+            Console.WriteLine("\n-------------------------------------------------");
+            Console.WriteLine("Conversion of decimal number to roman.");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine(MathConvertor.DecToRoman(3269));
+            
+
+            Console.WriteLine("\n-------------------------------------------------");
+            Console.WriteLine("Conversion of roman number to decimal.");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine(MathConvertor.RomanToDec("XLMCD"));
+            
+
+            Console.WriteLine("\n-------------------------------------------------");
+            Console.WriteLine("Solving of a quadratic equation.");
+            Console.WriteLine("-------------------------------------------------");
+            ExtraMath.SolveQuadraticEquation(1,-3,-4);
+
+            Console.WriteLine("\n-------------------------------------------------");
+            Console.WriteLine("Generation of random double.");
+            Console.WriteLine("-------------------------------------------------");
+            ExtraMath.GenerateRndNum(new Random(), 0.56256, 10.581254);
         }
     }
 }
