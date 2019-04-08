7f703c2b7d9fd2af97058314b8c23f5cef33fb89
st52531 <st52531@student.upce.cz>
2019-04-08 19:17:14 +0200

    Exercise08

---
+++ Exercise08Solution/Exercise08/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Exercise08
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chooseDirBtn = new System.Windows.Forms.Button();
+            this.chosenDirTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.infoListBox = new System.Windows.Forms.ListBox();
+            this.saveToFileCheckBox = new System.Windows.Forms.CheckBox();
+            this.showDirInfoCheckBox = new System.Windows.Forms.CheckBox();
+            this.analyseBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chooseDirBtn
+            // 
+            this.chooseDirBtn.Location = new System.Drawing.Point(546, 74);
+            this.chooseDirBtn.Name = "chooseDirBtn";
+            this.chooseDirBtn.Size = new System.Drawing.Size(127, 23);
+            this.chooseDirBtn.TabIndex = 0;
+            this.chooseDirBtn.Text = "Choose directory";
+            this.chooseDirBtn.UseVisualStyleBackColor = true;
+            this.chooseDirBtn.Click += new System.EventHandler(this.chooseDirBtn_Click);
+            // 
+            // chosenDirTextBox
+            // 
+            this.chosenDirTextBox.Location = new System.Drawing.Point(148, 74);
+            this.chosenDirTextBox.Name = "chosenDirTextBox";
+            this.chosenDirTextBox.Size = new System.Drawing.Size(374, 22);
+            this.chosenDirTextBox.TabIndex = 1;
+            this.chosenDirTextBox.Text = ".";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Chosen directory:";
+            // 
+            // infoListBox
+            // 
+            this.infoListBox.FormattingEnabled = true;
+            this.infoListBox.HorizontalScrollbar = true;
+            this.infoListBox.ItemHeight = 16;
+            this.infoListBox.Location = new System.Drawing.Point(15, 170);
+            this.infoListBox.Name = "infoListBox";
+            this.infoListBox.ScrollAlwaysVisible = true;
+            this.infoListBox.Size = new System.Drawing.Size(677, 260);
+            this.infoListBox.TabIndex = 3;
+            // 
+            // saveToFileCheckBox
+            // 
+            this.saveToFileCheckBox.AutoSize = true;
+            this.saveToFileCheckBox.Location = new System.Drawing.Point(159, 130);
+            this.saveToFileCheckBox.Name = "saveToFileCheckBox";
+            this.saveToFileCheckBox.Size = new System.Drawing.Size(100, 21);
+            this.saveToFileCheckBox.TabIndex = 4;
+            this.saveToFileCheckBox.Text = "Save to file";
+            this.saveToFileCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // showDirInfoCheckBox
+            // 
+            this.showDirInfoCheckBox.AutoSize = true;
+            this.showDirInfoCheckBox.Location = new System.Drawing.Point(15, 130);
+            this.showDirInfoCheckBox.Name = "showDirInfoCheckBox";
+            this.showDirInfoCheckBox.Size = new System.Drawing.Size(138, 21);
+            this.showDirInfoCheckBox.TabIndex = 5;
+            this.showDirInfoCheckBox.Text = "Show information";
+            this.showDirInfoCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // analyseBtn
+            // 
+            this.analyseBtn.Location = new System.Drawing.Point(569, 130);
+            this.analyseBtn.Name = "analyseBtn";
+            this.analyseBtn.Size = new System.Drawing.Size(75, 23);
+            this.analyseBtn.TabIndex = 6;
+            this.analyseBtn.Text = "Analyse";
+            this.analyseBtn.UseVisualStyleBackColor = true;
+            this.analyseBtn.Click += new System.EventHandler(this.AnalyseBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 450);
+            this.Controls.Add(this.analyseBtn);
+            this.Controls.Add(this.showDirInfoCheckBox);
+            this.Controls.Add(this.saveToFileCheckBox);
+            this.Controls.Add(this.infoListBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chosenDirTextBox);
+            this.Controls.Add(this.chooseDirBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button chooseDirBtn;
+        private System.Windows.Forms.TextBox chosenDirTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox infoListBox;
+        private System.Windows.Forms.CheckBox saveToFileCheckBox;
+        private System.Windows.Forms.CheckBox showDirInfoCheckBox;
+        private System.Windows.Forms.Button analyseBtn;
+    }
+}
+
---
+++ Exercise08Solution/Exercise08/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise08
+{
+    public partial class Form1 : Form
+    {
+        private FileInfo[] files;
+        private string[] subDirectories;
+        private FolderBrowserDialog fbd;
+        private DirectoryInfo d;
+        private string currentPath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chooseDirBtn_Click(object sender, EventArgs e)
+        {
+            using (fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    chosenDirTextBox.Text = fbd.SelectedPath;
+
+                    currentPath = fbd.SelectedPath;
+                    d = new DirectoryInfo(currentPath);
+
+                }
+            }
+        }
+
+        private void AnalyseBtn_Click(object sender, EventArgs e)
+        {
+            string[] fileExtensions = new string[20];
+
+            infoListBox.Items.Clear();
+            if (d == null)
+            {
+                currentPath = ".";
+                d = new DirectoryInfo(currentPath);
+            }
+
+            files = d.GetFiles();
+            subDirectories = Directory.GetDirectories(currentPath);
+
+            foreach (var dir in Directory.GetDirectories(currentPath))
+            {
+
+            }
+
+            foreach (FileInfo file in files)
+            {
+                for (int i = 0; i < fileExtensions.Length; i++)
+                {
+                    if (file.Extension != fileExtensions[i])
+                    {
+
+                    }
+                }
+
+            }
+
+            if (showDirInfoCheckBox.Checked)
+            {
+                infoListBox.Items.Add(files.Length.ToString());
+                for (int i = 0; i < files.Length; i++)
+                {
+                    infoListBox.Items.Add(files[i].Name);
+                }
+                for (int i = 0; i < subDirectories.Length; i++)
+                {
+                    infoListBox.Items.Add(subDirectories[i]);
+
+                }
+            }
+
+            if (saveToFileCheckBox.Checked)
+            {
+                using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(currentPath + "\\AnalysisInfo.txt"))
+                {
+                    file.WriteLine(files.Length.ToString());
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        file.WriteLine(files[i].Name);
+                    }
+                }
+            }
+        }
+    }
+}
