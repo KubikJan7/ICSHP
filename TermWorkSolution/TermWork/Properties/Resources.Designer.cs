52dd47c033401a5774ec0793d180be5b03245dfc
st52531 <st52531@student.upce.cz>
2019-04-08 13:14:17 +0200

    Term work background

--- TermWorkSolution/TermWork/Properties/Resources.Designer.cs
+++ TermWorkSolution/TermWork/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Exercise07Task01.Properties
+namespace TermWork.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace Exercise07Task01.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Exercise07Task01.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TermWork.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- TermWorkSolution/TermWork/Properties/Settings.Designer.cs
+++ TermWorkSolution/TermWork/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Exercise07Task01.Properties
+namespace TermWork.Properties
 {
 
 
