138a1a30b556090c16c72c746d3aa89a597ac5ca
st52531 <43739550+st52531@users.noreply.github.com>
2019-06-19 20:57:41 +0200

    Load/Save to file

--- EncryptionSolution/Encryption/Program.cs
+++ EncryptionSolution/Encryption/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,12 +29,13 @@ namespace Encryption
         static private (int, int, char[]) GetRowsAndColumns(string inputStr)
         {
             string str = inputStr.Replace(" ", string.Empty);
+            str = str.Replace("\n", string.Empty);
             int row, column;
             char[] charAr;
-            row = (int) Math.Sqrt(str.Length);
-            column = (int) Math.Ceiling(Math.Sqrt(str.Length));
+            row = (int)Math.Sqrt(str.Length);
+            column = (int)Math.Ceiling(Math.Sqrt(str.Length));
             charAr = str.ToCharArray();
-            return (row, column,charAr);
+            return (row, column, charAr);
         }
 
         static private string CreateEncodedString(Matrix<char> mat)
@@ -43,32 +45,66 @@ namespace Encryption
             {
                 for (int j = 0; j < mat.rows; j++)
                 {
-                    str+=mat.data[j, i];
+                    str += mat.data[j, i];
                 }
                 str += " ";
+                if (i % 4 == 0&&i!=0)
+                    str += "\r\n";
             }
             return str;
         }
 
+        private static string LoadStringFromFile(string textFile)
+        {
+            using (StreamReader file = new StreamReader(textFile))
+            {
+                string ln;
+                string str = "";
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    str += ln + "\n";
+                }
+                file.Close();
+                //Console.WriteLine(str);
+                return str;
+            }
+        }
+
+        private static void SaveMessageToFile(string textFile, string message)
+        {
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(textFile))
+            {
+                file.WriteLine(message);
+            }
+        }
+
         static void Main(string[] args)
         {
+            string textFile = "original_message.txt";
+
             Console.WriteLine("-------------------------------------------------------------------------");
             Console.WriteLine($"The purpose of this program is to encrypt a given string using a matrix.");
             Console.WriteLine("-------------------------------------------------------------------------");
 
-            Console.WriteLine($"Please type in a message to encrypt");
-            string inputStr = Console.ReadLine();
+            //Console.WriteLine($"Please type in a message to encrypt");
+            //string inputStr = Console.ReadLine();
             //Console.WriteLine($"The message to encode is following:\n{inputStr}\n");
+            string inputStr = LoadStringFromFile(textFile);
 
             int rows, columns;
             char[] charAr;
-            (rows,columns, charAr) = GetRowsAndColumns(inputStr);
-            Matrix<char> matrix = new Matrix<char>(rows,columns,charAr);
+            (rows, columns, charAr) = GetRowsAndColumns(inputStr);
+            Matrix<char> matrix = new Matrix<char>(rows, columns, charAr);
             Console.WriteLine("The matrix used for encoding looks like this:\n");
             matrix.DisplayMatrix();
 
             Console.WriteLine("\nThe message after encryption:\n");
-            Console.WriteLine($"{CreateEncodedString(matrix)}");
+            string encryptedMessage = CreateEncodedString(matrix);
+            Console.WriteLine($"{encryptedMessage}");
+            Console.WriteLine("\nThe message was saved into file 'encrypted_message.txt'\n");
+            SaveMessageToFile("ecrypted_message.txt", encryptedMessage);
         }
     }
 }
