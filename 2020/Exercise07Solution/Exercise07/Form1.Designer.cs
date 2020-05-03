a8def0644f25e16618408df73e2907f57dfc7eb9
KubikJan7 <43739550+st52531@users.noreply.github.com>
2020-05-03 18:45:20 +0200

    decoding in progress

--- 2020/Exercise07Solution/Exercise07/Form1.Designer.cs
+++ 2020/Exercise07Solution/Exercise07/Form1.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.imagePicBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
             this.loadFileBtn = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.imagePicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // imagePicBox
+            // pictureBox
             // 
-            this.imagePicBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.imagePicBox.BackColor = System.Drawing.SystemColors.Control;
-            this.imagePicBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.imagePicBox.Location = new System.Drawing.Point(-1, -1);
-            this.imagePicBox.Name = "imagePicBox";
-            this.imagePicBox.Size = new System.Drawing.Size(803, 419);
-            this.imagePicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.imagePicBox.TabIndex = 0;
-            this.imagePicBox.TabStop = false;
+            this.pictureBox.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(803, 419);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
             // 
             // loadFileBtn
             // 
@@ -57,7 +57,7 @@
             this.loadFileBtn.TabIndex = 1;
             this.loadFileBtn.Text = "Load file";
             this.loadFileBtn.UseVisualStyleBackColor = true;
-            this.loadFileBtn.Click += new System.EventHandler(this.loadFileBtn_Click);
+            this.loadFileBtn.Click += new System.EventHandler(this.LoadFileBtn_Click);
             // 
             // Form1
             // 
@@ -66,10 +66,10 @@
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.loadFileBtn);
-            this.Controls.Add(this.imagePicBox);
+            this.Controls.Add(this.pictureBox);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.imagePicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,7 +77,7 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox imagePicBox;
+        private System.Windows.Forms.PictureBox pictureBox;
         private System.Windows.Forms.Button loadFileBtn;
     }
 }
--- 2020/Exercise07Solution/Exercise07/Form1.cs
+++ 2020/Exercise07Solution/Exercise07/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,12 +20,85 @@ namespace Exercise07
 
         private Bitmap LoadPCX(string filename)
         {
-            return null;
+            byte[] imageData;
+            int width;
+            int height;
+            int scanLineLength;
+            int linePaddingSize;
+            int bitsPerPixel = 8;
+
+            using (BinaryReader br = new BinaryReader(File.OpenRead(filename)))
+            {
+                #region Reading the PCX file header
+                // Get the image width and height
+                br.BaseStream.Seek(4, SeekOrigin.Begin);
+                short xStart = br.ReadInt16();
+                short yStart = br.ReadInt16();
+                short xEnd = br.ReadInt16();
+                short yEnd = br.ReadInt16();
+                width = (xEnd - xStart + 1);
+                height = (yEnd - yStart + 1);
+
+                // Calculate the number of bytes required to hold a decoded scan line 
+                br.BaseStream.Seek(65, SeekOrigin.Begin);
+                byte numBitPlanes = br.ReadByte();
+                short bytesPerLine = br.ReadInt16();
+                scanLineLength = (numBitPlanes * bytesPerLine);
+
+                linePaddingSize = ((bytesPerLine * numBitPlanes) * (8 / bitsPerPixel)) - ((xEnd - yStart) + 1);
+                #endregion
+
+                #region Reading the PCX file data
+                imageData = new byte[scanLineLength];
+                byte @byte;
+                byte runCount;
+                byte runValue;
+                br.BaseStream.Seek(128, SeekOrigin.Begin);
+                for (int i = 0; i < imageData.Length; i++)
+                {
+                    @byte = br.ReadByte();
+                    if ((@byte & 0xC0) == 0xC0) // Two high bits are set
+                    {
+                        runCount = (byte)(@byte & 0x3F);
+                        runValue = br.ReadByte();
+                    }
+                    else
+                    {
+                        runCount = 1;
+                        runValue = @byte;
+                    }
+
+                    // Write the pixel run to the buffer
+                    while (runCount != 0)
+                    {
+                        if (i >= imageData.Length)
+                            break;
+                        imageData[i] = runValue;
+                        runCount--;
+                        i++;
+                    }
+                }
+
+                #endregion
+
+                br.Close();
+            }
+
+            //BinaryWriter bw = new BinaryWriter(File.OpenWrite("export.bmp"));
+            //bw.Write(imageData);
+            //bw.Close();
+
+
+            Bitmap bmp = new Bitmap(width, height);
+            using (var ms = new MemoryStream(imageData))
+            {
+                bmp = new Bitmap(ms);
+            }
+            return bmp;
         }
 
         private string ChooseFile()
         {
-            string filePath;
             using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
                 openFileDialog.Filter = "pcx files (*.pcx)|*.pcx";
@@ -33,16 +107,15 @@ namespace Exercise07
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
                     //Get the path of specified file
-                    filePath = openFileDialog.FileName;
-                    return filePath;
+                    return openFileDialog.FileName;
                 }
                 return string.Empty;
             }
         }
 
-        private void loadFileBtn_Click(object sender, EventArgs e)
+        private void LoadFileBtn_Click(object sender, EventArgs e)
         {
-            LoadPCX(ChooseFile());
+            pictureBox.Image = LoadPCX(ChooseFile());
         }
     }
 }
