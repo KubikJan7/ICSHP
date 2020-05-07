cdbc507e630ec666802184da5103824d71ea5496
KubikJan7 <st52531@student.upce.cz>
2020-05-07 07:10:56 +0200

    ExamSolution

---
+++ 2020/ExamSolution/Exam/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Threading;
+
+namespace Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
